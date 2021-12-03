6e7b7879172ca0239b1bf386a688efa979000f6d
Muhammed Said Kaya <saidkaya1239@gmail.com>
2021-12-04 01:27:43 +0300

    reorganized project structure

--- terraform/rt-subnet-association.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_route_table_association" "packer-rt-ig-association-1-test" {
-  subnet_id      = aws_subnet.packer-public-subnet-test.id
-  route_table_id = aws_route_table.packer-public_rt-test.id
-}
